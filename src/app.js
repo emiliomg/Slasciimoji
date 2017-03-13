65bd02ddf536bc365abdb006c6a8270a8bbe9fff
Emilio Markgraf <emilio.markgraf@fashionid.de>
2017-03-13 11:48:30 +0100

    browser base test route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.post('/ascii', function (req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World!');
+})
+
+app.post('/ascii', function (req, res) {
+  res.send('Hello Slack!');
 });
 
 app.listen(8000, function () {

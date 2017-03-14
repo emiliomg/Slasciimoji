6c005ed8b2eb39848ee36cd1dcba2a5bf65a1909
Emilio Markgraf <emilio.markgraf@fashionid.de>
2017-03-14 08:54:54 +0100

    experiment with request parameters

--- src/app.js
+++ src/app.js
@@ -7,10 +7,24 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 })
 
-app.post('/ascii', function (req, res) {
+app.post('/request', function (req, res) {
+  console.log('request body', req.query);
   res.send('Hello Slack!');
 });
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ', app.get('port'));
 });
+
+
+
+/**
+sir         | ( ಠ ͜ʖರೃ)
+zoidberg    | (\\/)(°,,,°)(\\/)
+eyes        | ಠ_ಠ
+sunglasses  | (⌐■_■)
+do it       | (☞ﾟヮﾟ)☞
+flip        | (╯°□°）╯︵ ┻━┻
+unflip      | ┬──┬◡ﾉ(° -°ﾉ)
+y u no      | ლ(ಠ益ಠლ)
+*/
